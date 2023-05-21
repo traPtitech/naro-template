6cc3a4239c0cf2067e572a7f9485197372ffcab0
mehm8128 <mehm8128@gmail.com>
2023-05-21 23:32:17 +0900

    間違えた

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -21,8 +21,8 @@ const addItem = () => {
 <template>
   <div>
     <div>ItemList</div>
-    <ul v-for="item in items" :key="item.name">
-      <li :class="{ over500: item.price >= 500 }">
+    <ul>
+      <li v-for="item in items" :key="item.name" :class="{ over500: item.price >= 500 }">
         <div>名前: {{ item.name }}</div>
         <div>{{ item.price }} 円</div>
         <div v-if="item.price >= 10000">高額商品</div>
