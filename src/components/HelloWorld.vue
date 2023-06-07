1f228a40133cbc185aa39bac6fdfebb32464d893
mehm8128 <mehm8128@gmail.com>
2023-06-07 20:09:21 +0900

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,29 @@
 <script setup lang="ts">
+import ClickCounter from './ClickCounter.vue'
+import ItemList from './ItemList.vue'
+import TodoList from './TodoList.vue'
+
 defineProps<{
   msg: string
 }>()
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
+  <div class="components">
+    <h1>{{ msg }}</h1>
+    <ClickCounter />
+    <ItemList />
+    <TodoList />
   </div>
 </template>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  top: -10px;
+a {
+  color: #42b983;
 }
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
+.components {
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
 }
 </style>
