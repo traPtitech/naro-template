627004fecb49f1e0dbe4aa4056450deb16a67b42
mehm8128 <mehm8128@gmail.com>
2023-06-13 22:24:12 +0900

    fix

--- src/components/ClickCounter.tsx
+++ src/components/ClickCounter.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 
 function ClickCounter() {
-	const [count, setCount] = useState(0)
+	const [count, setCount] = useState<number>(0)
 	return (
 		<div>
 			<div>回数： {count}</div>
