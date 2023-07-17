ae0f8307384440caf55e85f4ba35dda6f1bd3073
nokhnaton <sage_of_forest@icloud.com>
2023-07-17 15:00:44 +0900

    add beforeEach

--- src/router.ts
+++ src/router.ts
@@ -7,17 +7,31 @@ import CityPage from './pages/CityPage.vue'
 import CitySelect from './pages/CitySelect.vue'
 
 const routes = [
-  { path: '/', name: 'home', component: HomePage },
+  { path: '/', name: 'home', component: HomePage, meta: { isPublic: true } },
   { path: '/ping', name: 'ping', component: PingPage },
-  { path: '/login', name: 'login', component: LoginPage },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginPage,
+    meta: { isPublic: true }
+  },
   { path: '/city/:cityName', name: 'city', component: CityPage, props: true },
   { path: '/cityselect', name: 'cityselect', component: CitySelect },
-  { path: '/:path(.*)', component: NotFound }
+  { path: '/:path(.*)', component: NotFound, meta: { isPublic: true } }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+router.beforeEach(async (to) => {
+  if (to.meta.isPublic) {
+    return true
+  }
+  const res = await fetch('/api/whoami')
+  if (res.ok) return true
+  return '/login'
 })
 
 export default router
